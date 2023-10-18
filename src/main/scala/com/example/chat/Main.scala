1174d7e89d96abc945cd2926b2359957c94a9fd6
Aliaksei Suslau <aliaksei.suslau02@gmail.com>
2023-10-18 21:20:51 +0200

    Took the state away from the stream logic

--- src/main/scala/com/example/chat/Main.scala
+++ src/main/scala/com/example/chat/Main.scala
@@ -3,6 +3,7 @@ package com.example.chat
 import cats.effect.{ IO, IOApp }
 import cats.effect.std.Queue
 import fs2.concurrent.Topic
+import cats.effect.kernel.Ref
 
 case class State(messageCount: Int)
 
@@ -14,17 +15,18 @@ object Main extends IOApp.Simple:
     for
       queue <- Queue.unbounded[IO, FromClient]
       topic <- Topic[IO, ToClient]
+      ref   <- Ref[IO].of(State(0))
       _ <-
         fs2.Stream
           .fromQueueUnterminated(queue)
-          .mapAccumulate(State(0)) {
-            case (currentState, fromClient) =>
+          .evalMap(fromClient =>
+            ref.modify { currentState =>
               (
                 State(currentState.messageCount + 1),
                 ToClient(s"(${currentState.messageCount + 1}): ${fromClient.userName} - ${fromClient.message}")
               )
-          }
-          .map(_._2)
+            }
+          )
           .through(topic.publish)
           .compile.drain.start
       _ <- ChatServer.run[IO](queue, topic)
